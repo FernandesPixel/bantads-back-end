1400780627f91c0802791007da60bb89939f143e
Juan Fernandes <juanfernandesrrm@gmail.com>
2023-06-04 16:27:26 -0300

    Add HATEOAS in project

--- src/main/java/com/tads/bantads/controller/CustomerController.java
+++ src/main/java/com/tads/bantads/controller/CustomerController.java
@@ -3,11 +3,17 @@ package com.tads.bantads.controller;
 import com.tads.bantads.dto.CustomerDTO;
 import com.tads.bantads.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.Optional;
 import java.util.UUID;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/customer")
@@ -17,8 +23,13 @@ public class CustomerController {
     private CustomerService customerService;
 
     @PostMapping
-    public ResponseEntity<CustomerDTO> createCustomer(@RequestBody CustomerDTO customerDTO){
-        return ResponseEntity.ok().body(customerService.createCustomer(customerDTO));
+    public ResponseEntity<EntityModel<CustomerDTO>> createCustomer(@RequestBody CustomerDTO customerDTO) {
+        CustomerDTO createdCustomer = customerService.createCustomer(customerDTO);
+
+        EntityModel<CustomerDTO> customerResource = EntityModel.of(createdCustomer);
+        customerResource.add(linkTo(methodOn(CustomerController.class).getCustomer(createdCustomer.id())).withSelfRel());
+
+        return ResponseEntity.created(URI.create(customerResource.getRequiredLink(IanaLinkRelations.SELF).getHref())).body(customerResource);
     }
 
     @GetMapping("/{id}")

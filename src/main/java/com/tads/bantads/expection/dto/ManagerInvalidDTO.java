9e38beaef93a827d45ef357daa69669f443cf479
Gabriel <gabriel-borba@outlook.com>
2023-06-22 19:40:27 -0300

    Revert "Adicionando serviço de gerente"
    This reverts commit 9f436ee20658cd78fe186f087e8cdd6fbef04fbd.

--- src/main/java/com/tads/bantads/expection/dto/ManagerInvalidDTO.java
+++
@@ -1,9 +0,0 @@
-package com.tads.bantads.expection.dto;
-
-import org.springframework.validation.FieldError;
-
-public record ManagerInvalidDTO(String field, String message){
-    public ManagerInvalidDTO(FieldError error){
-        this(error.getField(), error.getDefaultMessage());
-    }
-}
